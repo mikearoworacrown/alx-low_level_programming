20c63a3a1c04c56d4913f71e163aa7e2ae26c773
Michael Aroworade <Ayoaro85@gmail.com>
2022-12-15 01:49:33 -0800

    0. Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * positive_or_negative - Assign random number when excuted
+ * Return: 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d is %s\n", i, "negative");
+	}
+	else if (i > 0)
+	{
+		printf("%d is %s\n", i, "positive");
+	}
+	else
+	{
+		printf("%d is %s\n", i, "zero");
+	}
+}
